19fbdbbcdd236e857efe7d6e441f02a7d9c7b53b
anibalUniversales <aemunozh@universales.com>
2023-06-20 11:22:57 -0600

    Initial Commit

---
+++ Ptzi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Ptzi
+//
+//  Created by Anibal Estuardo Munoz Hernandez on 20/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ptzi/PtziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PtziApp.swift
+//  Ptzi
+//
+//  Created by Anibal Estuardo Munoz Hernandez on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PtziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
